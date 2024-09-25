a9e193a7da03c161c5c9ebc7ca4574068a0af94e
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-09-25 13:56:25 +1000

    complete till bottom tabs

---
+++ app/(auth)/_layout.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+const AuthLayout = () => {
+    return (
+        <Text>
+
+        </Text>
+    );
+};
+
+export default AuthLayout;
---
+++ app/(tabs)/bookmark.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Bookmark = () => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default Bookmark;
