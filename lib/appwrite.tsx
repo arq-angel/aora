f081469004cc1d59aee4022675d51757baece295
arq-angel <87254753+arq-angel@users.noreply.github.com>
2024-09-25 16:50:29 +1000

    commit before installing appwrite in the app

---
+++ lib/appwrite.tsx
@@ -0,0 +1,9 @@
+export const appwriteConfig = {
+    endpoint: "https://cloud.appwrite.io/v1",
+    platform: "com.myapplib.aora",
+    projectId: "66f3af89002c63d6d038",
+    databaseId: "66f3b0950025b082e7f6",
+    userCollectionId: "66f3b0d6001f82b9a0db",
+    videoCollectionId: "66f3b0ee00021f42de45",
+    storageId: "66f3b1e3000c4986e6e1",
+}
